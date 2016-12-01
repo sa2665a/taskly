22720032369054c3f5ba74756102418f33de44f0
Simon Acosta <acosta1204@gmail.com>
2016-12-01 15:26:42 +0100

    Taskys exercises week 6 Ironhack

---
+++ taskly_ironhack_mail/spec/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,7 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+	def welcome_preview
+	UserMailer.welcome(User.first)
+  end
+
+end
